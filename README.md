06902893733e57d8ab073bc23bd0dcff8c9d4cc4
Ivan Verduzco <ivanverduzco11@gmail.com>
2024-03-13 19:54:31 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # manim2reel
  Creates videos from manim library.
+
+## Requisitos
+- [Python 3.11](https://www.python.org/downloads/)
+- [FFmpeg](https://www.ffmpeg.org/download.html)
+
+## Ambiente
+    
+```bash
+conda create -n manimv python=3.11
+conda activate manimv
+conda install pip
+conda install ffmpeg
+pip install -r requirements.txt
+```
+
+## Manim
+
+Ejecución de la aplicación:
+
+```bash
+
+```
---
+++ examples.py
@@ -0,0 +1,22 @@
+from manim import *
+
+class CreateGraph(Scene):
+    def construct(self):
+        axes = Axes(
+            x_range=[-3, 3],
+            y_range=[-5, 5],
+            axis_config={"color": BLUE},
+        )
+
+        # Create Graph
+        graph = axes.plot(lambda x: x**2, color=WHITE)
+        graph_label = axes.get_graph_label(graph, label='x^{2}')
+
+        graph2 = axes.plot(lambda x: x**3, color=WHITE)
+        graph_label2 = axes.get_graph_label(graph2, label='x^{3}')
+
+        # Display graph
+        self.play(Create(axes), Create(graph), Write(graph_label))
+        self.wait(1)
+        self.play(Transform(graph, graph2), Transform(graph_label, graph_label2))
+        self.wait(1)
